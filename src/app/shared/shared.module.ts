8b3f303dece641a11128949119c9c1f8f7cdcb1b
Paschalis Zaparas <pasx.zap@gmail.com>
2023-08-23 20:21:06 +0300

    half progress on contact form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { MaterialModule } from './material/material/material.module';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,10 +12,12 @@ import { MaterialModule } from './material/material/material.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MaterialModule
   ],
   exports: [
     FooterComponent,
+    FormsModule,
     TopBarComponent,
     MaterialModule
   ] 
